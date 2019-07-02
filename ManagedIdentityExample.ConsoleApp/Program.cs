8be00bdb5730f89a1b8e8f5c3aeeba8fbf7995f5
Rick van den Bosch <rickvdbosch@outlook.com>
2019-07-02 23:06:37 +0200

    Inital project setup Implemented getting secret from ConsoleApp using managed identity

---
+++ ManagedIdentityExample.ConsoleApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Services.AppAuthentication;
+
+namespace ManagedIdentityExample.ConsoleApp
+{
+    class Program
+    {
+        private const string SECRET_NAME = "SomeSecret";
+        private const string VAULT_URL = "https://mi-example.vault.azure.net/";
+
+        static async Task Main(string[] args)
+        {
+            var tokenProvider = new AzureServiceTokenProvider();
+            var kvc = new KeyVaultClient(
+                new KeyVaultClient.AuthenticationCallback(
+                    tokenProvider.KeyVaultTokenCallback));
+
+            var x = await kvc.GetSecretAsync(VAULT_URL, SECRET_NAME);
+            Console.WriteLine($"The secret we got from Key Vault: {x.Value}");
+            Console.ReadLine();
+        }
+    }
+}

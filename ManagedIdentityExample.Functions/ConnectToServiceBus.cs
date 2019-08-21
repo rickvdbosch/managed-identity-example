f77891f3c6b56c192a2741cb1d94c589c9c05140
Rick van den Bosch <rickvdbosch@outlook.com>
2019-08-21 22:44:07 +0200

    Implemented connecting to Service Bus using a Managed Identity

---
+++ ManagedIdentityExample.Functions/ConnectToServiceBus.cs
@@ -0,0 +1,46 @@
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.ServiceBus;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+
+using ManagedIdentityExample.Functions.TokenProviders;
+
+namespace ManagedIdentityExample.Functions
+{
+    public static class ConnectToServiceBus
+    {
+        #region Constants
+
+        private const string NAMESPACE = "https://<YOUR_NAMESPACE>.servicebus.windows.net";
+        private const string QUEUE_NAME = "demoq";
+        // TENANT_ID can both be the Directory ID (GUID) or the 'xxx.onmicrosoft.com' name of the tenant.
+        private const string TENANT_ID = "<YOUR_TENANT_ID>";
+
+        #endregion
+
+        [FunctionName("ConnectToServiceBus")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            string messageContent;
+
+            using (var streamReader = new StreamReader(req.Body))
+            {
+                messageContent = await streamReader.ReadToEndAsync();
+            }
+
+            var queueClient = new QueueClient(NAMESPACE, QUEUE_NAME, new ServiceBusTokenProvider(TENANT_ID));
+            var message = new Message(Encoding.UTF8.GetBytes(messageContent));
+            await queueClient.SendAsync(message);
+
+            return new OkResult();
+        }
+    }
+}
---
+++ ManagedIdentityExample.Functions/TokenProviders/ServiceBusTokenProvider.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using Microsoft.Azure.ServiceBus.Primitives;
+using Microsoft.Azure.Services.AppAuthentication;
+
+namespace ManagedIdentityExample.Functions.TokenProviders
+{
+    public class ServiceBusTokenProvider : TokenProvider
+    {
+        #region Constants
+
+        private const string RESOURCE_SERVICE_BUS = "https://servicebus.azure.net/";
+
+        #endregion
+
+        #region Fields
+
+        private string _tenantId;
+
+        #endregion
+
+        #region Constructors
+
+        public ServiceBusTokenProvider(string tenantId = null)
+        {
+            _tenantId = tenantId;
+        }
+
+        #endregion
+
+        public override async Task<SecurityToken> GetTokenAsync(string appliesTo, TimeSpan timeout)
+        {
+            var tokenProvider = new AzureServiceTokenProvider();
+            var token = await tokenProvider.GetAccessTokenAsync(RESOURCE_SERVICE_BUS, _tenantId);
+
+            return new JsonSecurityToken(token, appliesTo);
+        }
+    }
+}
